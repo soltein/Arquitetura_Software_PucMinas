ebde98d8841aea9259dd0d3db9179ed2d455c5d2
Julio Cezar <julio@soltein.com.br>
2023-06-17 22:41:45 -0300

    teste

--- src/main/java/br/com/popfood/controller/ClienteController.java
+++ src/main/java/br/com/popfood/controller/ClienteController.java
@@ -1,12 +1,12 @@
 package br.com.popfood.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ClienteController {
 
-	@GetMapping("cliente")
+	@GetMapping("/cliente")
 	public String cliente() {
 		return "teste";
 	}
---
+++ src/test/java/br/com/popfood/PopFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.popfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
